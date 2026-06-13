6035d97ea9d10b58d8633c03d5afd6a7b61e52ca
UltimateCoder-Sohail <sohailakhtar11sci@gmail.com>
2026-06-13 15:46:05 +0530

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
 import { GlassesTryOnStudio } from "../features/glasses-tryon/GlassesTryOnStudio";
+import Link from "next/link";
 
 export default function Home() {
-  return <GlassesTryOnStudio />;
+  return (
+    <div>
+      <GlassesTryOnStudio />
+
+      <div style={{ display: "flex", justifyContent: "flex-end" }}>
+        Powered By<Link href="/" style={{ textDecoration: "none", color: "blue" }}>
+          Ultimate Coder
+        </Link>
+      </div>
+    </div>
+  );
 }
